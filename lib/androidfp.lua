ec81e5da2223e330578d2e6cb521e56334cfee0d
Vk <vk15@tuta.io>
2026-06-20 23:26:54 -0300

    Create androidfp.lua

---
+++ lib/androidfp.lua
@@ -0,0 +1,67 @@
+local ffi = require("ffi")
+
+ffi.cdef[[
+    typedef void* jclass;
+    typedef void* jobject;
+    typedef void* jmethodID;
+    typedef void* jstring;
+    typedef union jvalue {
+        uint8_t z; int8_t b; uint16_t c; int16_t s;
+        int32_t i; int64_t j; float f; double d; void* l;
+    } jvalue;
+    void* SDL_AndroidGetJNIEnv(void);
+    void* SDL_AndroidGetActivity(void);
+]]
+
+local AndroidFilePicker = {}
+AndroidFilePicker.__index = AndroidFilePicker
+
+function AndroidFilePicker.new()
+    local self = setmetatable({}, AndroidFilePicker)
+    self.env= ffi.C.SDL_AndroidGetJNIEnv()
+    self.activity = ffi.C.SDL_AndroidGetActivity()
+    local vtable  = ffi.cast("void***", self.env)[0]
+    self.FindClass = ffi.cast("jclass  (*)(void*, const char*)", vtable[6])
+    self.DeleteLocalRef = ffi.cast("void    (*)(void*, jobject)",  vtable[21])
+    self.NewObjectA= ffi.cast("jobject (*)(void*, jclass, jmethodID, jvalue*)", vtable[28])
+    self.GetObjectClass = ffi.cast("jclass  (*)(void*, jobject)", vtable[29])
+    self.GetMethodID= ffi.cast("jmethodID (*)(void*, jclass, const char*, const char*)", vtable[31])
+    self.CallObjectMethodA= ffi.cast("jobject (*)(void*, jobject, jmethodID, jvalue*)", vtable[34])
+    self.CallVoidMethodA= ffi.cast("void    (*)(void*, jobject,    jmethodID, jvalue*)", vtable[61])
+    self.NewStringUTF= ffi.cast("jstring (*)(void*, const char*)", vtable[167])
+    return self
+end
+
+function AndroidFilePicker:openZIP()
+    local env = self.env
+
+    local intentClass  = self.FindClass(env, "android/content/Intent")
+    local intentInit = self.GetMethodID(env, intentClass, "<init>", "(Ljava/lang/String;)V")
+    local actionStr = self.NewStringUTF(env, "android.intent.action.OPEN_DOCUMENT")
+    local args1 = ffi.new("jvalue[1]"); args1[0].l = actionStr
+    local intentObj = self.NewObjectA(env, intentClass, intentInit, args1)
+
+    local setType  = self.GetMethodID(env, intentClass, "setType", "(Ljava/lang/String;)Landroid/content/Intent;")
+    local typeStr= self.NewStringUTF(env, "application/zip")
+    local args2 = ffi.new("jvalue[1]"); args2[0].l = typeStr
+    self.CallObjectMethodA(env, intentObj, setType, args2)
+
+    local addCat = self.GetMethodID(env, intentClass, "addCategory", "(Ljava/lang/String;)Landroid/content/Intent;")
+    local catStr = self.NewStringUTF(env, "android.intent.category.OPENABLE")
+    local args3 = ffi.new("jvalue[1]"); args3[0].l = catStr
+    self.CallObjectMethodA(env, intentObj, addCat, args3)
+
+    local actClass= self.GetObjectClass(env, self.activity)
+    local startMethod = self.GetMethodID(env, actClass, "startActivityForResult", "(Landroid/content/Intent;I)V")
+    local args4 = ffi.new("jvalue[2]"); args4[0].l = intentObj; args4[1].i = 42
+    self.CallVoidMethodA(env, self.activity, startMethod, args4)
+
+    self.DeleteLocalRef(env, actionStr)
+    self.DeleteLocalRef(env, typeStr)
+    self.DeleteLocalRef(env, catStr)
+    self.DeleteLocalRef(env, intentClass)
+    self.DeleteLocalRef(env, intentObj)
+    self.DeleteLocalRef(env, actClass)
+end
+
+return AndroidFilePicker
